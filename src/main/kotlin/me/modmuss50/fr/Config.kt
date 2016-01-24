2a1a60716b2cbfff48cfcca8548f79f5b7f072bc
modmuss50 <modmuss50@gmail.com>
2016-01-24 11:37:57 +0000

    Prep for update

---
+++ src/main/kotlin/me/modmuss50/fr/Config.kt
@@ -0,0 +1,21 @@
+package me.modmuss50.fr
+
+import net.minecraftforge.common.config.Configuration
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
+import java.io.File
+
+/**
+ * Created by modmuss50 on 24/01/2016 for Fluxed-Redstone.
+ */
+class Config(file: File?) : Configuration(file) {
+
+    fun preInit(event : FMLPreInitializationEvent){
+        this.load()
+        for(pipe in PipeTypeEnum.values){
+            pipe.maxRF = getInt(pipe.friendlyName, "Energy", pipe.defualtRF, 0, Int.MAX_VALUE, "Change the amount of energy the ${pipe.friendlyName} Wire can transfer per tick")
+        }
+        this.save()
+    }
+
+
+}
